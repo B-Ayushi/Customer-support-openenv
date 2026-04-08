4819e989942874a6b6e282825543f736c3074ff7
AyushiBindroo <bindrooayushi1@gmail.com>
2026-04-08 05:39:50 +0530

    fix server entrypoint for validator

--- server/app.py
+++ server/app.py
@@ -1,3 +1,10 @@
 from server.main import app
+import uvicorn
 
-__all__ = ["app"]
+
+def main():
+    uvicorn.run("server.main:app", host="0.0.0.0", port=7860, reload=False)
+
+
+if __name__ == "__main__":
+    main()
